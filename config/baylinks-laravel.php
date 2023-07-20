2ce50f05f47e5fab739ef9002ae7ac7bd8edda3d
palPalani <pal@targetbay.com>
2023-07-20 18:58:46 +0530

    :shirt: Improved and fixed Coding Standard related changes!

--- config/baylinks-laravel.php
+++ config/baylinks-laravel.php
@@ -2,5 +2,11 @@
 
 // config for PalPalani/BayLinks
 return [
+    'server' => env('BAYLINKS_SERVER'),
 
+    'api' => [
+        'url' => 'api/v1',
+        'key' => env('BAYLINKS_API_KEY'),
+        'secret' => env('BAYLINKS_API_SECRET'),
+    ],
 ];
